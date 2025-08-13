bd051ad8b20358837a1ecf28d538736145650fc0
Pradip <pradin.karki59@gmail.com>
2025-08-13 17:06:25 +0545

    Update DBUtil.java

--- src/main/java/com/example/quiz/dao/DBUtil.java
+++ src/main/java/com/example/quiz/dao/DBUtil.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/schema.sql"; // Replace with your DB name
-    private static final String USER = "postgres"; // Replace with your DB username
-    private static final String PASSWORD = "localhost"; // Replace with your DB password
+    private static final String USER = "Enter_your_Username"; // Replace with your DB username
+    private static final String PASSWORD = "Enter_your_password"; // Replace with your DB password
 
     static {
         try {
